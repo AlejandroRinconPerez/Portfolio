76e9434e61d745d8a5441275eeb24440b5948b12
AlejandroRinconPerez <alejandro.ingenieria.estudios@gmail.com>
2025-01-08 09:14:37 -0500

    feat: :sparkles: nex functions trying to creactive things
    slider section and temporary animation added correctly they need some adjust
    
    @AlejandroRinconPerez

--- main.js
+++ main.js
@@ -10,3 +10,36 @@ toggleBtn.onclick = function () {
 };
 
 
+
+
+
+const words = ["me", "projects", "skills", "tools"];
+let currentIndex = 0;
+
+
+const wordElement = document.querySelector(".word");
+
+
+function rotateWords() {
+  wordElement.textContent = words[currentIndex];
+  
+  wordElement.style.color = getRandomColor();
+
+  
+  currentIndex = (currentIndex + 1) % words.length;
+}
+
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+rotateWords();
+setInterval(rotateWords, 2000);
+
