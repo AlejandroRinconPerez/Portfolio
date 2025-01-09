139f6ba68d746cad509c364db07a28a282e1fd60
AlejandroRinconPerez <alejandro.ingenieria.estudios@gmail.com>
2025-01-09 09:20:10 -0500

    feat: :sparkles: new animation and styles
    new zones were done  with js and css
    
    @AlejandroRincon

--- main.js
+++ main.js
@@ -13,7 +13,9 @@ toggleBtn.onclick = function () {
 
 
 
-const words = ["me", "projects", "skills", "tools"];
+const words = ["Html", "CSS","Javascript", "Python", "Java","SQL","PostgreSQL","GitHub","Markdown"];
+
+
 let currentIndex = 0;
 
 
@@ -43,3 +45,52 @@ function getRandomColor() {
 rotateWords();
 setInterval(rotateWords, 2000);
 
+// Projecst 
+
+
+let items = document.querySelectorAll(".slider .item");
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
+
+let active =3;
+
+function loadShow(){
+  let stt = 0;
+  items[active].style.transform = `none`;
+  items[active].style.zIndex = 1;
+  items[active].style.filter = `none`
+  items[active].style.opacity = 1;
+
+
+  for(var i = active + 1; i < items.length; i++){
+    stt++;
+    items[i].style.transform = `translateX(${stt * 120}px) scale(${1 - 0.2*stt}) perspective(16px)  rotateY(-1deg)`;
+    items[i].style.zIndex = -stt;
+    items[i].style.filter = `blur(2px)`
+    items[i].style.opacity = stt > 2 ? 0 : 0.6;
+
+}
+   stt = 0;
+  for(var i = active - 1; i >= 0; i--){
+    stt++;
+    items[i].style.transform = `translateX(${stt * -120}px) scale(${1 - 0.2*stt}) perspective(16px)  rotateY(1deg)`;
+    items[i].style.zIndex = -stt;
+    items[i].style.filter = `blur(2px)`
+    items[i].style.opacity = stt > 2 ? 0 : 0.6;
+
+}
+
+}
+
+loadShow();
+
+
+next.onclick = function(){
+  active = active + 1 < items.length ? active + 1 : active;
+  loadShow();
+}
+
+prev.onclick = function(){
+  active = active - 1 >= 0  ? active - 1 : active;
+  loadShow();
+}
