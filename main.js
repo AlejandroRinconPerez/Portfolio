a1eacf0515a34f133af5d6c1d287bf26dff71b73
AlejandroRinconPerez <alejandro.ingenieria.estudios@gmail.com>
2025-01-10 15:23:37 -0500

    feat: :zap: new zone aBout me has been remake to one more organized
    new about me zopne with new animations and effects
    
    @AlejandroRincon

--- main.js
+++ main.js
@@ -1,36 +1,53 @@
-const toggleBtn = document.querySelector('.toggle');
-const toggleBtnIcon = document.querySelector('.toggle ion-icon');
-const dropDownMenu = document.querySelector('.dropdown_menu');
+const toggleBtn = document.querySelector(".toggle");
+const toggleBtnIcon = document.querySelector(".toggle ion-icon");
+const dropDownMenu = document.querySelector(".dropdown_menu");
 
 toggleBtn.onclick = function () {
-    dropDownMenu.classList.toggle('open');
-    const isOpen = dropDownMenu.classList.contains('open');
+  dropDownMenu.classList.toggle("open");
+  const isOpen = dropDownMenu.classList.contains("open");
 
-    toggleBtnIcon.setAttribute('name', isOpen ? 'close-outline' : 'menu-outline');
+  toggleBtnIcon.setAttribute("name", isOpen ? "close-outline" : "menu-outline");
 };
 
-
-
-
-
-const words = ["Html", "CSS","Javascript", "Python", "Java","SQL","PostgreSQL","GitHub","Markdown"];
-
+const words = [
+  "Html",
+  "CSS",
+  "Javascript",
+  "Python",
+  "Java",
+  "SQL",
+  "PostgreSQL",
+  "GitHub",
+  "Markdown",
+  "Git",
+];
 
 let currentIndex = 0;
 
-
 const wordElement = document.querySelector(".word");
-
 
 function rotateWords() {
   wordElement.textContent = words[currentIndex];
-  
+
   wordElement.style.color = getRandomColor();
 
-  
   currentIndex = (currentIndex + 1) % words.length;
 }
 
+function newhover() {
+  const spans = document.querySelectorAll(".scroll div span");
+
+  spans.forEach((span) => {
+    span.addEventListener("mouseover", function () {
+      if (span.style.background != "transparent") {
+        span.style.background = "1a1225";
+      }
+
+      span.style.background = getRandomColor();
+    });
+  });
+}
+newhover();
 
 function getRandomColor() {
   const letters = "0123456789ABCDEF";
@@ -41,56 +58,53 @@ function getRandomColor() {
   return color;
 }
 
-
 rotateWords();
 setInterval(rotateWords, 2000);
 
-// Projecst 
-
+// Projecst
 
 let items = document.querySelectorAll(".slider .item");
 let next = document.getElementById("next");
 let prev = document.getElementById("prev");
 
-let active =3;
+let active = 3;
 
-function loadShow(){
+function loadShow() {
   let stt = 0;
   items[active].style.transform = `none`;
   items[active].style.zIndex = 1;
-  items[active].style.filter = `none`
+  items[active].style.filter = `none`;
   items[active].style.opacity = 1;
 
-
-  for(var i = active + 1; i < items.length; i++){
+  for (var i = active + 1; i < items.length; i++) {
     stt++;
-    items[i].style.transform = `translateX(${stt * 120}px) scale(${1 - 0.2*stt}) perspective(16px)  rotateY(-1deg)`;
+    items[i].style.transform = `translateX(${stt * 120}px) scale(${
+      1 - 0.2 * stt
+    }) perspective(16px)  rotateY(-1deg)`;
     items[i].style.zIndex = -stt;
-    items[i].style.filter = `blur(2px)`
+    items[i].style.filter = `blur(2px)`;
     items[i].style.opacity = stt > 2 ? 0 : 0.6;
-
-}
-   stt = 0;
-  for(var i = active - 1; i >= 0; i--){
+  }
+  stt = 0;
+  for (var i = active - 1; i >= 0; i--) {
     stt++;
-    items[i].style.transform = `translateX(${stt * -120}px) scale(${1 - 0.2*stt}) perspective(16px)  rotateY(1deg)`;
+    items[i].style.transform = `translateX(${stt * -120}px) scale(${
+      1 - 0.2 * stt
+    }) perspective(16px)  rotateY(1deg)`;
     items[i].style.zIndex = -stt;
-    items[i].style.filter = `blur(2px)`
+    items[i].style.filter = `blur(2px)`;
     items[i].style.opacity = stt > 2 ? 0 : 0.6;
-
-}
-
+  }
 }
 
 loadShow();
 
-
-next.onclick = function(){
+next.onclick = function () {
   active = active + 1 < items.length ? active + 1 : active;
   loadShow();
-}
+};
 
-prev.onclick = function(){
-  active = active - 1 >= 0  ? active - 1 : active;
+prev.onclick = function () {
+  active = active - 1 >= 0 ? active - 1 : active;
   loadShow();
-}
+};
